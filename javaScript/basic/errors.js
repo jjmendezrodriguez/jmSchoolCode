63504d7015aa3b3792f62ec46cf2b7acc0c180e5
Jose Mendez <jjmendezrodriguez@yahoo.com>
2024-10-28 01:08:22 -0400

    file move

---
+++ javaScript/basic/errors.js
@@ -0,0 +1,7 @@
+/*
+Los syntax errors o errores de sintaxis son:
+Errores en el código que ocurren cuando el intérprete
+o compilador encuentra algo mal escrito o que no sigue
+las reglas del lenguaje de programación. Estos errores
+previenen que el programa se ejecute hasta que sean corregidos. 
+*/
---
+++ javaScript/basic/math.js
@@ -0,0 +1,13 @@
+// str need ""
+// num not
+
+let firstBatch = 5;
+let secondBatch = 7;
+//  ^ Variables whit num values.
+
+// cada simbolo matematio tiene una accion (*, +, -, /, %)
+let count = firstBatch + secondBatch;
+// In this case sum. but can add two str.
+
+console.log(count);
+// Print 12
