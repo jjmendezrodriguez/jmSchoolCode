ea1d5c325930fcb30bfb2c2574f51c84a0ebd44b
Jose Mendez <jjmendezrodriguez@yahoo.com>
2025-03-08 15:38:13 -0400

    level_2_update march

--- javaScript/level_2_update/index.js
+++ javaScript/level_2_update/index.js
@@ -8,7 +8,6 @@ let message = ""; // El propocito de esta variable es para cambiar el mensaje en
 let player = {
   name: "",
   chips: 0,
-  lastTen: false,
 };
 
 // Aqui seleccionas el id del HTML
@@ -18,21 +17,34 @@ let cardsEl = document.getElementById("cards-el");
 let betEl = document.getElementById("bet-el");
 let playerEl = document.getElementById("player-el");
 
-const startGame = document.getElementById("start-game");
+// Main menu ========================================
+
 const mainMenu = document.getElementById("main-menu");
-const gameStart = document.querySelector(".game-container");
-const alertOne = document.getElementById("alert-one");
 let nameInput = document.getElementById("player-input");
+let isStart = true;
+
+const startGame = document.getElementById("start-game");
+const alertOne = document.getElementById("alert-one");
+const playerName = document.getElementById("player-name");
+let playerChips = document.getElementById("player-chips");
 
 startGame.addEventListener("click", function (e) {
   e.preventDefault();
-  if (nameInput.value.trim() === "") {
-    alertOne.classList.add("active");
+  if (isStart) {
+    nameInput.classList.add("active");
+    this.textContent = "Submit";
+    isStart = false;
   } else {
-    player.name = nameInput.value.trim();
-    playerEl.textContent = `name: ${player.name} Chips: $${player.chips}`;
-    mainMenu.classList.add("deactive");
-    console.log(player);
+    if (nameInput.value.trim() === "") {
+      alertOne.classList.add("active");
+      nameInput.focus();
+    } else {
+      player.name = nameInput.value.trim();
+      playerName.textContent = `Name: ${player.name}`;
+      playerChips.textContent = `Chips: $${player.chips}`;
+      mainMenu.classList.add("deactive");
+      console.log(player);
+    }
   }
 });
 
@@ -42,6 +54,24 @@ nameInput.addEventListener("input", () => {
     alertOne.classList.remove("active"); // Remover rojo si el campo es válido
   }
 });
+
+// Alert variable ========================================
+
+const alertMsg = document.getElementById("alert-msg");
+let timeout;
+
+function showAlert(msg = "Aletr", onOff = true) {
+  alertMsg.classList.add("active");
+  alertMsg.textContent = msg;
+  if (onOff) {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      alertMsg.classList.remove("active");
+    }, 3000);
+  }
+}
+
+// Random card deck ========================================
 
 function getRamdomCard() {
   let randomNumber = Math.floor(Math.random() * 13) + 1; // guardamos el valor de Math.random() en una variable
@@ -55,153 +85,162 @@ function getRamdomCard() {
   return randomNumber; // por ser una variable let y local, podemos usarla en cualquier parte de la funcion.
 }
 
-function start() {}
+// Quit game ==========================================
 
-function quit() {
-  isAlive = false;
-  Object.assign(player, { name: "", chips: 0, lastTen: false });
-  nameInput.value = "";
-  mainMenu.classList.remove("deactive");
-  console.log(player);
-}
+const quitGame = document.getElementById("quit-btn");
 
-function startPlay() {
-  isAlive = true; // Para que el juego inicie debe ser true.
-  console.log("voy por aqui");
-  if (player.chips < 10) {
-    while (player.chips < 10) {
-      alert("You need at least $10 to play!");
-      player.chips = prompt("How many chips do you want to add?");
-      playerEl.textContent = player.name.toUpperCase() + ": $" + player.chips;
-    }
-  }
-  if (cards > 0) {
-    startOver();
+quitGame.addEventListener("click", function () {
+  // isAlive = false;
+  // Object.assign(player, { name: "", chips: 0 });
+  // nameInput.value = "";
+  // playerName.textContent = `Name: ${player.name}`;
+  // playerChips.textContent = `Chips: $${player.chips}`;
+  // mainMenu.classList.remove("deactive");
+  // nameInput.classList.remove("active");
+  // startGame.textContent = "START GAME";
+  // isStart = true;
+  // console.log(isAlive + player + "click btn quit");
+  location.reload();
+});
+
+// Start play ========================================
+
+const startPlayBtn = document.getElementById("start-play");
+
+startPlayBtn.addEventListener("click", function () {
+  if (player.chips <= 0) {
+    showAlert("Please Add Credit To Your Accound!", false);
+  } else if (isAlive === false) {
+    showAlert("click new game!", false);
   } else {
+    isAlive = true; // Para que el juego inicie debe ser true.
     let firsCard = getRamdomCard();
     let secondCard = getRamdomCard();
     cards = [firsCard, secondCard];
     /* ^ cambiamos el valor del array con los valores de firsCard y secondCard,
   de esta forma podemos saber que cartas se han sacado al inicio del juego. */
     sum = firsCard + secondCard;
-    renderGame();
-    console.log("comienza el juego");
+    renderGame(true);
+    console.log(`${isAlive} click star play btn`);
     // Con esta funcion iniciamos el juego con dos cartas y el valor ramdom de cada carta.
   }
-}
+});
+
+// chips ========================================
+
+const ChipsBtn = document.getElementById("chips-btn");
+
+ChipsBtn.addEventListener("click", addChips);
 
 function addChips() {
-  console.log("addChips");
-  if (player.chips < 10) {
-    while (player.chips < 10) {
-      alert("You need at least $10 to play!");
-      player.chips = prompt("How many chips do you want to add?");
-      playerEl.textContent = player.name.toUpperCase() + ": $" + player.chips;
-    }
-  }
+  player.chips += 10;
+  console.log(player); // para pruebas
+  playerChips.textContent = `Chips: $${player.chips}`;
+  alertMsg.classList.remove("active");
+  isAlive = true;
 }
 
 function stay() {}
 
+// Bet ========================================
+
+const betBtn = document.getElementById("bet-btn");
+
+betBtn.addEventListener("click", bet);
+
 function bet() {
-  console.log("click bet");
+  console.log(`${isAlive} click bet btn`);
   if (isAlive === false) {
-    alert("You need to start a new game!");
-  }
-  if (player.chips >= 10) {
-    // rev. to move to startGame()
-    alert("you bet $10");
-    player.chips -= 10;
-    playerEl.textContent = player.name.toUpperCase() + ": $" + player.chips;
-    startGame();
-  }
-  if (cards > 1 && player.chips < 10) {
-    //fix
-    addChips();
-  }
-}
-
-function newGame() {
-  if (isAlive === false) {
-    alert("You need to start a new game!");
+    showAlert("click new game!", false);
+  } else if (player.chips <= 0) {
+    showAlert("Please Add Credit To Your Accound!");
   } else {
-    startOver();
-    cards = [];
-    sum = 0;
-    hasBlasckJack = false;
-    isAlive = false;
-    message = "";
-    renderGame();
+    sumBet += 10;
+    player.chips -= 10;
+    playerChips.textContent = `Chips: $${player.chips}`;
+    betEl.textContent = `Bet: $${sumBet}`;
   }
 }
 
-function renderGame() {
-  cardsEl.textContent = "Cards: ";
-  for (let i = 0; i < cards.length; i++) {
-    cardsEl.textContent += cards[i] + " ";
-  }
-  sumEL.textContent = "Sum: " + sum;
+// Render game ========================================
 
-  sumBet += 10;
-  player.chips -= 10;
-  sumBet += 10;
-  playerEl.textContent = player.name.toUpperCase() + ": $" + player.chips;
-  betEl.textContent = "Bet: " + sumBet;
+function renderGame(isNewRound = false) {
+  cardsEl.textContent = `Cards: `;
+  for (let i of cards) {
+    cardsEl.textContent += `${i} `;
+  }
+  sumEL.textContent = `Sum: ${sum}`;
+
+  if (isNewRound) {
+    console.log(isNewRound + "new Round");
+    sumBet += 10;
+    player.chips -= 10;
+    sumBet += 10;
+    playerChips.textContent = `Chips: $${player.chips}`;
+    betEl.textContent = `Bet: $${sumBet}`;
+  }
 
   if (sum <= 20) {
     message = "Do you want to draw a new card?";
     isAlive = true;
+    console.log("voy por if < 20");
   } else if (sum === 21) {
     message = "Wohoo! You've got Blackjack!";
     hasBlasckJack = true;
     player.chips += sumBet;
     sumBet = 0;
-    playerEl.textContent = player.name.toUpperCase() + ": $" + player.chips;
-    betEl.textContent = "Bet: " + sumBet;
+    playerName.textContent = `Name: ${player.name}`;
+    playerChips.textContent = `Chips: $${player.chips}`;
+    betEl.textContent = `Bet: $${sumBet}`;
+    isAlive = false;
   } else {
     message = "You're out of the game!";
     isAlive = false;
+    showAlert("you lose, start new game!", false);
   }
   //Con esto cambias el texto del HTML, la linea donde coinside el id usado.
   messageEl.textContent = message;
 }
 
+// New card ========================================
+
+const newCardBtn = document.getElementById("new-card");
+
+newCardBtn.addEventListener("click", newCard);
+
 function newCard() {
-  if (!isAlive || sum > 10) {
-    alert("You need to start a new game!");
+  console.log(`${isAlive} ${hasBlasckJack} new card btn`);
+  if (isAlive === false) {
+    showAlert("You need to start the game!");
   } else if (isAlive && hasBlasckJack === false) {
     // si isAlive es true y hasBlasckJack es false
     let card = getRamdomCard(); // Creamos una nueva carta
     sum += card; // sum el nuevo valor al anterior valor de sum
     cards.push(card);
+    console.log(`${isAlive} ${hasBlasckJack} click new card btn else `);
     renderGame(); /* Para activar la funcion startGame(). 
     No olvidemos que cuando llamamos una funcion se ejecuta todo
     lo que esta dentro de las llaves {}, por eso llamamos StartGame(). */
   }
 }
 
-function startOver() {
-  if (!isAlive || sum === 0) {
-    alert("1 You need to start a new game!");
-  } else if (isAlive) {
-    response = prompt("Are you sure you want to start over? yes or no");
-    /* ^ guardamos el valor de prompt en una variable, para poder 
-    usarla en el if statement. */
-    if (response === "yes") {
-      reset();
-    } else if (response === "no") {
-      console.log("Ok, keep playing!");
-    }
-    while (response !== "yes" && response !== "no") {
-      alert("Please enter yes or no");
-      response = prompt("Are you sure you want to start over? yes or no");
-    }
-  }
+// New game ========================================
+
+const newGameBtn = document.getElementById("new-game");
+
+newGameBtn.addEventListener("click", newGame);
+
+function newGame() {
+  isAlive = true;
+  hasBlasckJack = false;
+  console.log(`${isAlive} ${hasBlasckJack} click newgamebtn`);
+  alertMsg.classList.remove("active");
 }
 
 function reset() {
   newGame();
-  player.name = [];
-  player.chips = [];
-  playerEl.textContent = player.name + "" + player.chips;
+  player.name = "";
+  player.chips = 0;
+  playerName.textContent = `Name: ${player.name}`;
+  playerChips.textContent = `Chips: $${player.chips}`;
 }
--- react/react-app/src/main.tsx
+++ react/react-app/src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom/client"; // para cambiar a native
 import App from "./App.tsx";
-// import './index.css' esta línea no es necesaria se usa si estas usando los estilos de index.css en tu proyecto.
+// import './index.css' <= esto es necesario se usa si estas usando los estilos de index.css en tu proyecto.
 import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
